59406fd1860fa0406a3442e7b2de9c31a1e0da6e
willian95 <rodriguezwillian95@gmail.com>
2021-07-23 20:02:46 -0400

    cambios

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\SendEmailController;
 
 
 /*
@@ -16,3 +17,5 @@ use App\Http\Controllers\IndexController;
 */
 
 Route::get('/', [IndexController::class, "index"]);
+
+Route::post("/send/message", [SendEmailController::class, "sendEmail"]);
